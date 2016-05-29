82312f8c4204dc8f96ffc814cb1ad28232d3d5da
Jonathan Luck <jrluck@hotmail.com>
2016-05-28 19:16:25 -0700

    Initial files Files: -index.py -- server -index.html -- client -js/scraper.js -- client js

---
+++ index.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import requests
+
+def formatreq(args):
+    defaults ={'amount': 1, 'categ' : 'All', 'sub' : 'None', 'qtype' : "Tossups", 'difficulty' : "College", 'tournamentyear':'All'}
+    querryarr = []
+    #for some reason it isn't retaining it's order when being changed to a list. sorted() to maintain consistent order?
+    for k in sorted(list(defaults.keys())):
+        querryarr.append(args.get(k,defaults[k]))
+    if (int(querryarr[0]) > 20):
+        querryarr[0] = "20"
+    a = '/php/randomResults.php?amount={}&categ={}&difficulty={}&qtype={}&sub={}&tournamentyear={}'.format(*querryarr)
+    return a
+    
+app = Flask("Test")
+@app.route('/')
+def index():
+    return open('index.html', 'r').read()
+
+@app.route('/js/<path:path>')
+def js(path):
+    return open("js/"+ path.strip(".."),'r').read()
+
+@app.route('/random/')
+def rando():
+    querry = formatreq(request.args)
+    print(querry)
+    r = requests.get("http://quinterest.org{}".format(querry))
+    return r.text
+
+
+app.run("localhost",8080)
