f0a29b58070f49aff3344b96d1b5d610c7a8f5b4
Jonathan Luck <jrluck@hotmail.com>
2016-05-30 16:38:20 -0700

    Bolding powermarked questions Also increased font size of the result questions

--- index.py
+++ index.py
@@ -4,12 +4,11 @@ from requests import get
 from urllib.parse import parse_qs
 from bs4 import BeautifulSoup
 from random import shuffle
-import re
+from re import sub
 
 def formatreq(args):
     defaults ={'amount': [1], 'categ' : ['All'], 'sub' : ['None'], 'qtype' : ["Tossups"], 'difficulty' : ["College"], 'tournamentyear':['All']}
     args = parse_qs(args);
-    print("args: ", args)
     querryarr = []
     #for some reason it isn't retaining it's order when being changed to a list. sorted() to maintain consistent order?
     for k in sorted(list(defaults.keys())):
@@ -22,7 +21,13 @@ def formatreq(args):
 def processQuestions(arr):
     shuffle(arr)
     for i in range(len(arr)):
-        arr[i] = re.sub(r'Result: [0-9]+ \|', "Result: {} |".format(i+1), arr[i])
+        arr[i] = sub(r'Result: [0-9]+ \|', "Result: {} |".format(i+1), arr[i])
+        if(arr[i].find("(*)") > 0):
+            arr[i] = arr[i].replace("<em>Question:</em>", "<b>")
+            arr[i] = arr[i].replace("(*)" , "(*)</b>")
+        else:
+            arr[i] = arr[i].replace("<em>Question:</em>", "")
+            
     return arr
     
     
@@ -41,12 +46,10 @@ def js(path):
 def rando():
     #clip off the last semicolon then split up the separate querries
     reqs = request.query_string.decode('utf-8')[:-1].split(";")
-    print("reqs: ", reqs)
     querry = []
     #make a list of querries to quinterest
     for r in reqs:
         querry.append(formatreq(r))
-    print("querry: ", querry)
     questions = []
     for q in querry:
         out = get("http://quinterest.org{}".format(q)).text
