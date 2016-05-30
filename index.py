e5ceae35cbc9512b0e2b53e52e36f926fa7dce93
Jonathan Luck <jrluck@hotmail.com>
2016-05-30 15:56:58 -0700

    Main functionality done Added: - Server combines results of question requests into one set, then shuffles the question order and reformats. Todo: - Front end styling issues - Bold questions with powermarks - Find bug with removing rows

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 from flask.ext.compress import Compress
 from requests import get
 from urllib.parse import parse_qs
+from bs4 import BeautifulSoup
+from random import shuffle
+import re
 
 def formatreq(args):
     defaults ={'amount': [1], 'categ' : ['All'], 'sub' : ['None'], 'qtype' : ["Tossups"], 'difficulty' : ["College"], 'tournamentyear':['All']}
@@ -15,6 +18,13 @@ def formatreq(args):
         querryarr[0] = "20"
     a = '/php/randomResults.php?amount={}&categ={}&difficulty={}&qtype={}&sub={}&tournamentyear={}'.format(*querryarr)
     return a
+
+def processQuestions(arr):
+    shuffle(arr)
+    for i in range(len(arr)):
+        arr[i] = re.sub(r'Result: [0-9]+ \|', "Result: {} |".format(i+1), arr[i])
+    return arr
+    
     
 app = Flask("Test")
 Compress(app)
@@ -37,10 +47,15 @@ def rando():
     for r in reqs:
         querry.append(formatreq(r))
     print("querry: ", querry)
-    out = ""
+    questions = []
     for q in querry:
-        out += get("http://quinterest.org{}".format(q)).text
-    return out
+        out = get("http://quinterest.org{}".format(q)).text
+        out = BeautifulSoup(out, 'html.parser').find_all(attrs={"class":"row"})
+        out.pop(0)
+        for e in out:
+            questions.append(str(e))
+    questions = processQuestions(questions)
+    return ("<br>".join(questions))
 
 
 app.run("localhost",8080)
